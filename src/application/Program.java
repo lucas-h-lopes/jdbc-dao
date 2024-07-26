ca231134154e2e2b4265217f2b33f1f600f39254
Lucas <lucas.hen.lps@gmail.com>
2024-07-26 19:45:32 -0300

    feat: seller findById method

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,7 +1,16 @@
 package application;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
+import model.entities.Seller;
+
+import java.util.Locale;
+
 public class Program {
     public static void main(String[] args) {
-
+        Locale.setDefault(Locale.US);
+        SellerDao sellerDao = DaoFactory.createSellerDao();
+        Seller seller = sellerDao.findById(1);
+        System.out.println(seller);
     }
 }
--- src/model/dao/implementation/SellerDaoJDBC.java
+++ src/model/dao/implementation/SellerDaoJDBC.java
@@ -1,15 +1,21 @@
 package model.dao.implementation;
 
+import db.DbException;
 import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.time.LocalDateTime;
 import java.util.List;
 
 public class SellerDaoJDBC implements SellerDao {
     private Connection conn;
-    public SellerDaoJDBC(Connection conn){
+
+    public SellerDaoJDBC(Connection conn) {
         this.conn = conn;
     }
 
@@ -30,7 +36,21 @@ public class SellerDaoJDBC implements SellerDao {
 
     @Override
     public Seller findById(Integer id) {
-        return null;
+        PreparedStatement pStatement = null;
+        ResultSet resultSet = null;
+        String sql = "SELECT seller.*,department.Name as DepName FROM seller INNER JOIN department ON seller.DepartmentId = department.Id WHERE seller.Id = ?";
+        try {
+            pStatement = conn.prepareStatement(sql);
+            pStatement.setInt(1, id);
+            resultSet = pStatement.executeQuery();
+            if (resultSet.next()) {
+                return instantiateSeller(resultSet, new Department(resultSet.getInt("DepartmentId"), resultSet.getString("DepName")));
+            }
+            throw new DbException("Error: There is no Seller with Id = " + id);
+
+        } catch (SQLException e) {
+            throw new DbException("Error: " + e.getMessage());
+        }
     }
 
     @Override
@@ -41,5 +61,18 @@ public class SellerDaoJDBC implements SellerDao {
     @Override
     public List<Seller> findByDepartment(Department department) {
         return List.of();
+    }
+
+    private Seller instantiateSeller(ResultSet resultSet, Department department) throws SQLException {
+        return new Seller(resultSet.getInt("Id"),
+                resultSet.getString("Name"),
+                resultSet.getString("Email"),
+                resultSet.getObject("BirthDate", LocalDateTime.class),
+                resultSet.getDouble("BaseSalary"),
+                department);
+    }
+
+    private Department instantiateDepartment(ResultSet resultSet) throws SQLException {
+        return new Department(resultSet.getInt("Id"), resultSet.getString("Name"));
     }
 }
