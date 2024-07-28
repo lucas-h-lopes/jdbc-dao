881a340be063bb386dbab5fb68122e38fc2702aa
Lucas <lucas.hen.lps@gmail.com>
2024-07-28 17:33:25 -0300

    feat: modifying from Exception to RuntimeException

--- src/model/exceptions/ConsoleManagerException.java
+++ src/model/exceptions/ConsoleManagerException.java
@@ -2,7 +2,7 @@ package model.exceptions;
 
 import util.TextColor;
 
-public class ConsoleManagerException extends Exception{
+public class ConsoleManagerException extends RuntimeException{
     public ConsoleManagerException(String msg){
         super(TextColor.formatToRed(msg));
     }
